dda76b7b83749381add721aa3f683b00c481fd63
danieljax88 <danieljax88@hotmail.co.uk>
2022-09-08 16:35:02 +0100

    Page optimisation, significant

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
+module.exports = withBundleAnalyzer({
   images: {
     domains: ['firebasestorage.googleapis.com'],
 
   },
   reactStrictMode: true,
 
-};
+});
