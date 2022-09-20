4f55235ec18e508f511b32f026a0d5519fc2867b
danieljax88 <danieljax88@hotmail.co.uk>
2022-09-20 09:49:25 +0100

    Desserts fixed

--- pages/recipes/desserts.js
+++ pages/recipes/desserts.js
@@ -28,56 +28,65 @@ const Desserts = () => {
         const getDessertsFromFirebase = [];
         onSnapshot(firstBatch, (snapshot) => {
             snapshot.docs.forEach((doc) => {
-                const lastVisible = snapshot.docs[snapshot.docs.leng
+                const lastVisible = snapshot.docs[snapshot.docs.length - 1];
+                setLast(lastVisible.data())
+                getDessertsFromFirebase.push({ ...doc.data(), key: doc.id })
+            })
+            setDessertsRecipe(getDessertsFromFirebase)
+            setDessertsLoading(false)
+
+        })
+
+    }, [dessertsloading]);
 
     const fetchMoreData = () => {
 
-                    const field = "title"
-                    const next = query(docRef, orderBy(field, 'asc'), where("category", "==", 'Dessert'), startAfter(last[field]), limit(20))
-                    getDocs(next)
-                        .then((querySnapshot) => {
-                            const lastVisible = querySnapshot.docs[querySnapshot.docs.length - 1];
-                            const postList = []
-                            querySnapshot.forEach((doc) => {
-                                postList.push({ ...doc.data(), key: doc.id });
-                            })
+        const field = "title"
+        const next = query(docRef, orderBy(field, 'asc'), where("category", "==", 'Dessert'), startAfter(last[field]), limit(20))
+        getDocs(next)
+            .then((querySnapshot) => {
+                const lastVisible = querySnapshot.docs[querySnapshot.docs.length - 1];
+                const postList = []
+                querySnapshot.forEach((doc) => {
+                    postList.push({ ...doc.data(), key: doc.id });
+                })
 
-                            if (lastVisible !== undefined) {
-                                setDessertsRecipe([...dessertsRecipe, ...postList]);
-                                setLast(lastVisible.data());
+                if (lastVisible !== undefined) {
+                    setDessertsRecipe([...dessertsRecipe, ...postList]);
+                    setLast(lastVisible.data());
 
-                            } else {
-                                setNotify("nothing to load.");
-                                return;
-                            }
-
-                        })
-                        .catch((err) => {
-                            console.log(err)
-                        })
-                };
-
-                if (dessertsloading) {
-                    return (
-                        <Backdrop
-                            sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-                            open={dessertsloading}
-
-                        >
-                            <CircularProgress color="inherit" />
-                        </Backdrop>
-                    )
+                } else {
+                    setNotify("nothing to load.");
+                    return;
                 }
 
-                return (
-                    <InfiniteScroll
-                        dataLength={dessertsRecipe.length}
-                        next={fetchMoreData}
-                        hasMore={true}>
-                        <FoodCard recipes={dessertsRecipe} />
-                    </InfiniteScroll>
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    };
 
-                )
+    if (dessertsloading) {
+        return (
+            <Backdrop
+                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={dessertsloading}
 
-            }
+            >
+                <CircularProgress color="inherit" />
+            </Backdrop>
+        )
+    }
+
+    return (
+        <InfiniteScroll
+            dataLength={dessertsRecipe.length}
+            next={fetchMoreData}
+            hasMore={true}>
+            <FoodCard recipes={dessertsRecipe} />
+        </InfiniteScroll>
+
+    )
+
+}
 export default Desserts
